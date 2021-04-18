0e6bb0ec9a0a8f524ee97dba0e090b3f2c2d5489
lucianodelboni <lucknfx5@gmail.com>
2021-04-18 13:42:16 -0400

    creating portfolio_app directory and getting ready to create my first views

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio_app',
 ]
 
 MIDDLEWARE = [
@@ -73,16 +74,16 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rpffzdzk',
-        'USER': 'rpffzdzk',
-        'PASSWORD': 'qM0PF2COL_ICqIuRtzZ08XarfAH6ev9d',
-        'HOST': 'motty.db.elephantsql.com',
-        'PORT': '5432'
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'rpffzdzk',
+#        'USER': 'rpffzdzk',
+#        'PASSWORD': 'qM0PF2COL_ICqIuRtzZ08XarfAH6ev9d',
+#        'HOST': 'motty.db.elephantsql.com',
+#        'PORT': '5432'
+#    }
+#}
 
 
 # Password validation
