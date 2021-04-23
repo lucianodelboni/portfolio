c5a5ff973c567abb20f95c1b7af54e1000204c74
lucianodelboni <lucknfx5@gmail.com>
2021-04-23 13:52:51 -0400

    changing the project's structure to use template inheritance

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Portfolio/base_templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
