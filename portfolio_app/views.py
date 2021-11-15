17027f8f60456dc291eddc63b3caab0f5a7538e5
lucianodelboni <lucknfx5@gmail.com>
2021-11-15 10:05:47 -0400

    got contact form to work properly

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def landing_page(request):
     return render(request, 'landing_page.html',{})
     
